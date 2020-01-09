943e31c586ed35f493e1c182e85281fb071f38da
Kellen Presta <kpresta04@gmail.com>
2020-01-09 13:12:14 -0600

    added textareas

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   $(".event-td").on("click", function() {
+//     console.log(this);
+//     let enterField = $("<textarea>");
+//     $(this).append(enterField);
+//     $(enterField).text($(this).text());
+//     $(enterField).focus();
+//   });
+// });
