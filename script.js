9ed5cccbf06d900c0a6177d1eb5f6d3e226df9c5
Kellen Presta <kpresta04@gmail.com>
2020-01-09 13:37:27 -0600

    adding click event to buttons

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// $(document).ready(function() {
-//   $(".event-td").on("click", function() {
-//     console.log(this);
-//     let enterField = $("<textarea>");
-//     $(this).append(enterField);
-//     $(enterField).text($(this).text());
-//     $(enterField).focus();
-//   });
-// });
+$(document).ready(function() {
+  //get list of buttons from DOM
+  buttons = document.getElementsByClassName("save-button");
+
+  // buttons is a HTML collection, must convert to array first,
+  //then can use forEach
+  Array.from(buttons).forEach(function(button, i) {
+    //Add ID to each button
+    $(buttons[i]).attr("id", `button${i}`);
+    //add click event to each button
+    $(buttons[i]).on("click", function() {
+      console.log($(`#text${[i]}`).val());
+    });
+  });
+});
